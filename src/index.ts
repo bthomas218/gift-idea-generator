21eb43f8f63a2e6474c034d9971a3c7de744d7b0
brady <bradythomas690@gmail.com>
2026-04-07 20:51:20 -0500

    refactor: simplified serving the react app

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import config from "./cfg";
+import index from "./app/index.html";
+
+Bun.serve({
+  port: config.port,
+  hostname: config.hostname,
+
+  routes: {
+    "/*": index,
+  },
+});
+
+console.log(`Server running at http://${config.hostname}:${config.port}`);
